1c5cb118c6d63d36fe55d4913cd50ad561f6f5a8
ronykader <rony.ksr06@gmail.com>
2023-10-20 16:22:44 +0600

    slider: created slider options

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,20 +1,44 @@
 import Image from "next/image";
+const data = [
+    {
+      id: 1,
+      title: "always fresh & always crispy & always hot",
+      image: "/slide1.png",
+    },
+    {
+      id: 2,
+      title: "we deliver your order wherever you are in NY",
+      image: "/slide2.png",
+    },
+    {
+      id: 3,
+      title: "the best pizza to share with your family",
+      image: "/slide3.jpg",
+    },
+  ];
+
 
 const Slider = () => {
+    const [currentSlide, setCurrentSlide]
     return (
         <div>
             <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
       {/* TEXT CONTAINER */}
-      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
+      {data.map((item) => {
+       return <>
+        <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
         <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
-          Title
+          {item.title}
         </h1>
         <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
       </div>
       {/* IMAGE CONTAINER */}
       <div className="w-full flex-1 relative">
-        <Image src="/slide1.png" fill className="object-cover"/>
+        <Image src="/slide1.png" fill className="object-cover" alt=""/>
       </div>
+       </>
+      })}
+      
     </div>
         </div>
     );
